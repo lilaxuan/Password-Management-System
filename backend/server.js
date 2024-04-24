c6da0ba73b2d01bc0c41dc71c07d1d94bd8d415e
lilaxuan <106371956+lilaxuan@users.noreply.github.com>
2024-04-24 11:47:53 -0700

    Add CORS

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // const cookieParser = require('cookie-parser') // todo: import later on
 const app = express();
-const path = require('path')
+const path = require('path');
+const cors = require('cors');
 
 
 // Notes: When Creating db, no need to pre-set up schema
@@ -20,12 +21,12 @@ db.on('error', console.error.bind(console, 'Error connecting to MongoDB:'));
 // app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api/passwords', passwords); // defined the base root url for the API
 app.use('/api/users', users); // defined the base root url for the API
 app.use(bodyParser.json());
-
 app.use(express.static('public')); // use static files in public
+app.use(cors());
+
 
 // set up front end directory
 let frontend_dir = path.join(__dirname, '..', 'frontend', 'dist')
